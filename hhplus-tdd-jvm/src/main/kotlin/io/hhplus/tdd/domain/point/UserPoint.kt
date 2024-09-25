b2396accc8eb4d6612bbf89a0991152fd53898ab
profoundsea25 <profoundsea25@gmail.com>
2024-09-25 21:33:15 +0900

    feat: 포인트 최대/최소 추가

--- hhplus-tdd-jvm/src/main/kotlin/io/hhplus/tdd/domain/point/UserPoint.kt
+++ hhplus-tdd-jvm/src/main/kotlin/io/hhplus/tdd/domain/point/UserPoint.kt
@@ -4,4 +4,9 @@ data class UserPoint(
     val id: Long,
     val point: Long,
     val updateMillis: Long,
-)
+) {
+    companion object {
+        const val MAX_POINT_LIMIT: Long = 5_000_000L
+        const val MIN_POINT_LIMIT: Long = 0L
+    }
+}
