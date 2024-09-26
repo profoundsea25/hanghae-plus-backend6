2968f47bc17f989ca20ac488e4da2e38210b3f36
profoundsea25 <profoundsea25@gmail.com>
2024-09-26 21:06:11 +0900

    refactor: ReentrantLockManager 로직 보강
    - Lock 획득에 timeout 추가

--- hhplus-tdd-jvm/src/main/kotlin/io/hhplus/tdd/adapter/point/lock/ReentrantLockManager.kt
+++ hhplus-tdd-jvm/src/main/kotlin/io/hhplus/tdd/adapter/point/lock/ReentrantLockManager.kt
@@ -2,27 +2,26 @@ package io.hhplus.tdd.adapter.point.lock
 
 import io.hhplus.tdd.domain.point.out.LockManager
 import org.springframework.stereotype.Component
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.ConcurrentMap
+import java.util.concurrent.TimeUnit
 import java.util.concurrent.locks.ReentrantLock
 
 @Component
-class LockManagerImpl : LockManager {
+class ReentrantLockManager : LockManager {
     companion object {
-        val LOCK_MAP: ConcurrentMap<Long, ReentrantLock> = ConcurrentHashMap()
+        val LOCK = ReentrantLock()
     }
 
     override fun <T> lockWith(
         id: Long,
         block: () -> T,
     ): T {
-        val lock = LOCK_MAP.getOrPut(id) { ReentrantLock() }
-
-        lock.lock()
+        if (!LOCK.tryLock(3L, TimeUnit.SECONDS)) {
+            throw RuntimeException("Lock timeout")
+        }
         try {
             return block()
         } finally {
-            lock.unlock()
+            LOCK.unlock()
         }
     }
 }
